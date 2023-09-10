43759db28d41a5b000d62ca74201be7c7a61b051
Veilics <laputafelix@gmail.com>
2023-09-10 22:10:15 +0800

    /gptCamera integrated with Html5Qrcode

--- src/pages/gptCamera/index.tsx
+++ src/pages/gptCamera/index.tsx
@@ -1,5 +1,12 @@
-import React, { ReactElement, useEffect, useRef, useState } from "react";
-import { NextPageWithLayout } from "../_app";
+import { type ReactElement, useEffect, useRef, useState } from "react";
+
+import {
+  Html5Qrcode,
+  Html5QrcodeSupportedFormats,
+  type QrcodeErrorCallback,
+  type QrcodeSuccessCallback,
+} from "html5-qrcode";
+import { type NextPageWithLayout } from "../_app";
 import Layout from "~/layouts/productDetailLayout";
 
 const VideoPlayer: NextPageWithLayout = () => {
@@ -7,6 +14,15 @@ const VideoPlayer: NextPageWithLayout = () => {
   const [videoSource, setVideoSource] = useState<MediaStream | null>(null);
   const [videoDevices, setVideoDevices] = useState<MediaDeviceInfo[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isScanning, setIsScanning] = useState(false);
+
+  const onScanSuccessHandler: QrcodeSuccessCallback = (text, result) => {
+    console.log(text);
+    console.log(JSON.stringify(result, null, 2));
+  };
+  const onScanErrorHandler: QrcodeErrorCallback = (errorMessage, error) => {
+    console.error(errorMessage, error);
+  };
 
   useEffect(() => {
     if (videoRef.current && videoSource) {
@@ -21,7 +37,6 @@ const VideoPlayer: NextPageWithLayout = () => {
         const videoInputDevices = devices.filter(
           (device) => device.kind === "videoinput"
         );
-
         setVideoDevices(videoInputDevices);
       } catch (error) {
         console.error("Error accessing media devices:", error);
@@ -31,14 +46,37 @@ const VideoPlayer: NextPageWithLayout = () => {
     void getVideoDevices();
   }, []);
 
+  useEffect(() => {
+    if (videoRef.current) {
+      const html5QrCode = new Html5Qrcode("qr-code-reader");
+
+      const scanQRCode = async (videoElement: HTMLVideoElement) => {
+        try {
+          await html5QrCode.start(
+            videoElement,
+            { fps: 10, qrbox: 250 },
+            onScanSuccessHandler,
+            onScanErrorHandler
+          );
+        } catch (error) {
+          console.error("QR code scanning error:", error);
+        }
+      };
+
+      void scanQRCode(videoRef.current);
+
+      return () => {
+        html5QrCode.stop().catch((e) => console.error(e));
+      };
+    }
+  }, []);
+
   const handleVideoSourceChange = async (deviceId: string) => {
     try {
       setIsLoading(true);
-
-      const constraints: MediaStreamConstraints = {
+      const constraints = {
         video: {
-          deviceId: { ideal: deviceId },
-          facingMode: "environment",
+          deviceId: { exact: deviceId },
           aspectRatio: 2.5558,
         },
         audio: true,
@@ -68,26 +106,19 @@ const VideoPlayer: NextPageWithLayout = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-1">
+    <div className="flex flex-col items-center justify-center">
       <div className="">
         {isLoading && <div className="loading-spinner"></div>}
-        {videoRef ? (
-          <video
-            ref={videoRef}
-            autoPlay
-            playsInline
-            controls
-            key={videoSource?.id}
-          />
-        ) : null}
+        <video ref={videoRef} />
+        <div id="qr-code-reader"></div>
       </div>
       <div>{renderVideoSourceOptions()}</div>
     </div>
   );
 };
 
-export default VideoPlayer;
-
 VideoPlayer.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
 };
+
+export default VideoPlayer;
